bda2f28530df602d38e0d04011b4aa6127b9783b
Svi-Kos <svitlanakos@gmail.com>
2020-12-15 18:28:21 +0200

    рендер через статуси

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import Searchbar from './components/Searchbar/Searchbar';
 import ImageGallery from './components/ImageGallery/ImageGallery';
-import Button from './components/Button/Button';
-import Loader from 'react-loader-spinner';
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+
 import './App.css';
 
 class App extends Component {
@@ -19,15 +17,7 @@ class App extends Component {
     return (
       <div>
         <Searchbar onSubmit={this.handleFormSubmit} />
-        <Loader
-          type="ThreeDots"
-          color="#303f9f"
-          height={80}
-          width={80}
-          style={{ textAlign: 'center' }}
-        />
         <ImageGallery searchQuery={this.state.searchQuery} />
-        <Button />
       </div>
     );
   }
--- my-app/src/components/ImageGallery/ImageGallery.jsx
+++ my-app/src/components/ImageGallery/ImageGallery.jsx
@@ -1,63 +1,82 @@
 import React, { Component } from 'react';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+import Button from '../Button/Button';
+import Loader from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import s from '../ImageGallery/ImageGallery.module.css';
-// import * as basicLightbox from 'basiclightbox';
-// import 'basiclightbox/dist/basicLightbox.min.css';
-// import Loader from 'react-loader-spinner';
-// import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 class ImageGallery extends Component {
   state = {
     images: null,
+    error: null,
+    status: 'idle',
   };
+  // 'idle'
+  // 'pending'
+  // 'resolved'
+  // 'rejected'
 
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.searchQuery !== this.props.searchQuery) {
+      this.setState({ status: 'pending' });
       const url = `https://pixabay.com/api/?q=${this.props.searchQuery}&page=1&key=18966198-cc77d794ba7550ec695901208&image_type=photo&orientation=horizontal&per_page=12`;
+      const errorMessage = 'Please enter more specific query';
 
       fetch(url)
-        .then(response => response.json())
-        .then(images => this.setState({ images }))
-        .catch(error => console.log('error', error));
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          return Promise.reject(new Error(errorMessage));
+        })
+        .then(images => {
+          this.setState({ images, status: 'resolved' });
+          if (images.hits.length === 0) {
+            alert(errorMessage);
+          }
+        })
+        .catch(error => this.setState({ error, status: 'rejected' }));
     }
   }
 
-  // showLargeImage = event => {
-  //   const instance = basicLightbox.create(`
-  //     <img src="${event.target.alt}" alt="${event.target.alt}" width="800" height="600">
-  // `);
-
-  //   instance.show();
-  // };
-
   render() {
-    const { images } = this.state;
-    return (
-      <ul className={s.ImageGallery}>
-        <ImageGalleryItem images={images} />
-        {/* {images &&
-          images.hits.map(image => (
-            <li
-              className={s.ImageGalleryItem}
-              key={image.id}
-              onClick={this.showLargeImage}
-            >
-              <ImageGalleryItem
-                src={image.webformatURL}
-                alt={image.largeImageURL}
-              />
-            </li>
-          ))} */}
-      </ul>
-    );
+    const { images, error, status } = this.state;
+
+    if (status === 'idle') {
+      return <></>;
+    }
+
+    if (status === 'pending') {
+      return (
+        <Loader
+          type="ThreeDots"
+          color="#303f9f"
+          height={80}
+          width={80}
+          style={{ textAlign: 'center' }}
+        />
+      );
+    }
+
+    if (status === 'rejected') {
+      return <h1>{error.message}</h1>;
+    }
+
+    if (status === 'resolved') {
+      return (
+        <>
+          <ul className={s.ImageGallery}>
+            <ImageGalleryItem images={images} />
+          </ul>
+          <Button />
+        </>
+      );
+    }
   }
 }
 
-// зробити рендер через статуси
+// кнопка лишається після помилки
 
-// рендер кнопки після загрузки картинок
-// модалка?
-// спіннер
 // пропси
 
 export default ImageGallery;
